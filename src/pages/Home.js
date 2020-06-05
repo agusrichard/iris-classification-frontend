35a770f157a3c1ce1b61085a56161ef923a298b2
agusrichard <agus.richard21@gmail.com>
2020-06-05 12:55:10 +0700

    Custom Navbar

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CustomNavbar from '../components/CustomNavbar'
+
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <CustomNavbar />        
+      </>
+    )
+  }
+}
