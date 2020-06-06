d7325cfa2ddbca1fc99596be1eebed5e627afbdb
agusrichard <agus.richard21@gmail.com>
2020-06-06 11:31:15 +0700

    Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,19 +10,11 @@ export default class Home extends React.Component {
         <section className="home">
           <div className="row h-100 justify-content-md align-items-center">
             <div className="home-container">
-              <div style={{ width: '60%', margin: '20px' }}>
-                <p className="text-white text-md">
+              <div className="home-text-container">
+                <p className="text-white text-lg bolder">
                   You can’t connect the dots looking forward; you can only connect them looking backwards. So you have to trust that the dots will somehow connect in your future.
                 </p>
-                <p className="text-white text-lg">-- Steve Jobs</p>
-              </div>
-              <div style={{ width: '40%' }} className="links">
-                <div>
-                  <Link to="#">Register</Link>
-                </div>
-                <div>
-                  <Link to="#">Login</Link>
-                </div>
+                <p className="text-white text-xl bolder">- Steve Jobs</p>
               </div>
             </div>
           </div>
